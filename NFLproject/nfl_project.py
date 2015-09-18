55e91549cfaa3d236b547f9446ab4d1e036cd17d
Arjun Gupta <arjungupta291@berkeley.edu>
2015-09-18 01:56:18 -0700

    .

--- NFLproject/nfl_project.py
+++ NFLproject/nfl_project.py
@@ -1,5 +1,8 @@
 from urllib import request
+from bs4 import BeautifulSoup
 import json
+import heapq
+from pprint import pprint
 from scipy import stats
 
 teams = ['SF', 'CHI', 'CIN', 'BUF', 'DEN', 'CLE', 'TB', 'ARI',
@@ -16,6 +19,14 @@ url_form_stats = 'http://api.nfldata.apiphany.com/trial/json/TeamSeasonStats/201
 url_form_sched = 'http://api.nfldata.apiphany.com/trial/json/Schedules/2015?subscription-key=b7d211fd970143a2940d9905fe5c2447'
 
 ###Builder Functions###
+
+def truncate(f, n):
+    '''Truncates/pads a float f to n decimal places without rounding'''
+    s = '{}'.format(f)
+    if 'e' in s or 'E' in s:
+        return '{0:.{1}f}'.format(f, n)
+    i, p, d = s.partition('.')
+    return '.'.join([i, (d+'0'*n)[:n]])
 
 def pullSeasonData(year):
     # enter year as int in yyyy format
@@ -170,8 +181,52 @@ def gameWinProbability(year,team,opponent):
     distribution = stats.norm(line,13)
     winProbability = round((1 - distribution.cdf(0)), 2)
     return winProbability
+
+def assignOdds(data):
+    teams_unsorted = ['SF', 'CHI', 'CIN', 'BUF', 'DEN', 'CLE', 'TB', 'ARI',
+                      'SD', 'KC', 'IND', 'DAL', 'MIA', 'PHI', 'ATL', 'NYG',
+                      'JAX', 'NYJ', 'DET', 'GB', 'CAR', 'NE', 'OAK', 'STL',
+                      'BAL', 'WAS', 'NO', 'SEA', 'PIT', 'HOU', 'TEN', 'MIN']
+    team_info = {}
+    for team in teams_unsorted:
+        team_data = data.pop(0)
+        if team_data[0] == 'Chiefs':
+            team_info[team] = [float(team_data[1]), int(team_data[3][:4]), int(team_data[6][:4])]
+        elif len(team_data) == 10:
+            team_info[team] = [float(team_data[1]), int(team_data[4][:4]), int(team_data[6][:4])]
+        elif len(team_data) == 11:
+            team_info[team] = [float(team_data[1]), int(team_data[4][:4]), int(team_data[7][:4])]
+    return team_info
+
+def oddsScraper():
+    url = "http://linemakers.sportingnews.com/article/4635972-nfl-win-totals-2015-season-vegas-odds-seahawks-patriots-cowboys-saints-broncos"
+    content = request.urlopen(url)
+    html = content.read()
+    soup = BeautifulSoup(html, "html.parser")
+    info_location = soup.find_all('div', {'class': 'entry-content'})
+    general_odds_section = info_location[0].find_all('p')
+    odds_data = general_odds_section[4].find_all('br')
+    relevant_data = odds_data[0].get_text()
+    formatted_data = relevant_data.split('\n')
+    split_data = [item.split(" ") for item in formatted_data if item]
+    return assignOdds(split_data)
+    ## Returned in format {team: [expectedWins, 'Over', 'Under']} ##
+
+def vegasProbabilites(teamOdds):
+    for team in teamOdds:
+        over = teamOdds[team][1]
+        under = teamOdds[team][2]
+        if over < 0:
+            teamOdds[team][1] = round(((-(over)) / ((-(over)) + 100.0 )), 2)
+        else:
+            teamOdds[team][1] =  round((100.0 / (over + 100)), 2)
+        if under < 0:
+            teamOdds[team][2] = round(((-(under)) / ((-(under)) + 100.0 )), 2)
+        else:
+            teamOdds[team][2] = round((100.0 / (under + 100)), 2)
+    return teamOdds
     
-### Main Function ###
+### Main Functions ###
 
 def expectedWins(year):
     # enter year as int in yyyy format
@@ -195,10 +250,47 @@ def expectedWins(year):
         expectedWins[team] = int(round(sum(winProbabilities), 0))
     return expectedWins
 
+def compareProbabilities(year):
+    ## Expectation of a normal random variable suggests 50/50 over/under probabilites ##
+    teamWins = expectedWins(year)
+    teamOdds = oddsScraper()
+    vegasImpliedProbs = vegasProbabilites(teamOdds)
+    vegasValgoProbabilities = {}
+    for team in teamWins:
+        vegasExpectation = vegasImpliedProbs[team][0]
+        vegasDistribution = stats.norm(vegasExpectation, 2.5)
+        againstVegasUnderProbability = float(truncate(vegasDistribution.cdf(teamWins[team]), 2))
+        againstVegasOverProbability = float(truncate(1 - againstVegasUnderProbability, 2))
+        vegasValgoProbabilities[team] = [vegasImpliedProbs[team][0], againstVegasOverProbability, againstVegasUnderProbability]
+    return vegasValgoProbabilities, teamWins
+
+def generalRecommendations(year):
+    algoProbabilites, algoWins = compareProbabilities(year)
+    recommendations = {}
+    for team in algoProbabilites:
+        if abs(algoProbabilites[team][0] - algoWins[team]) < 1.5:
+            recommendations[team] = {"Algo Expected Wins": algoWins[team],
+                                     "Vegas Wins": algoProbabilites[team][0],
+                                     "Recommendation": "Avoid", 
+                                     "Edge": "Minimal/None"}
+        elif algoProbabilites[team][1] < 0.50:
+            recommendations[team] = {"Algo Expected Wins": algoWins[team],
+                                     "Vegas Wins": algoProbabilites[team][0],
+                                     "Recommendation": "Over", 
+                                     "Edge": truncate((algoProbabilites[team][2] - 0.50) * 100, 2) + "%"}
+        elif algoProbabilites[team][1] > 0.50:
+            recommendations[team] = {"Algo Expected Wins": algoWins[team],
+                                     "Vegas Wins": algoProbabilites[team][0],
+                                     "Recommendation": "Under", 
+                                     "Edge": truncate((algoProbabilites[team][1] - 0.50) * 100, 2) + "%"}
+    return recommendations
+
 
 if __name__ == '__main__':
-    year = int(input("Enter A Year to see Win Predictions: "))
-    print (expectedWins(year))
+    year = int(input("Enter current year to see betting recommendations: "))
+    pprint(generalRecommendations(year), width=1)
+
+   
 
 
         
